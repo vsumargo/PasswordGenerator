36e8e3b1e8385fef394f8b00616dcca1987979b8
Vsumargo <vincent.sumargo@gmail.com>
2020-11-25 13:24:03 +0800

    initial commit for fully functional random Password Generator

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # PasswordGenerator
+
+## Overview
+
+In this project, student create a random password generator with several criteria:
+* password must contain between 8 and 128 characters
+* decide if password want to include uppercase and/or numericals and/or symbols characters
+
+## Version History
+
+* **Version 0.0 - 25 November 2020**
+    Three initial source codes uploaded for the password generator:
+    * index.html
+    * style.css
+    * script.js
+
+## How it Works
+
+1. Find the link provided below on the "Screenshoot and Links" section titled "Password Generator"
+
+2. Click the link and open in browser
+
+3. First, decide how many characters the password want to contain. Use the slider button to adjust the number of characters or type numbers directly into the provided box
+
+4. Second, to include uppercase characters, tick the box on the right of "include uppercase", otherwise leave blank or untick
+
+5. Third, to include Numericals characters, tick the box on the right of "include numerics", otherwise leave blank or untick
+
+6. Fourth, to include Symbols characters, tick the box on the right of "include symbols", otherwise leave blank or untick
+
+7. After all criterias have been set, click the "Generate Password" button to generate a random password
+
+8. The random generated password will be displayed in the white box below the "Generate Password" button
+
+9. To Copy, use the mouse to click + hold and highlight the entire password, then right click mouse and copy or you can use the windows command ctrl+c to copy
+
+10. Paste the password into the desired place.
+
+
+## Screenshot and Links
+
+#### Screenshots
+The following image shows the three pages appearance:
+
+1. Password Generator ScreenShoot
+
+![screenshot of Password Generator](./Assets/Images/screenshoot-about-page.png)
+
+#### Links
+The following links will take you to the source code in github repository and the deployed live page:
+
+* [Password Generator](https://vsumargo.github.io/Bootstrap-Responsive-Page/)
+
+* [GitHub Source Code](https://github.com/vsumargo/Bootstrap-Responsive-Page)
+
+## Acknowledgements
+
+* **Trilogy Education Services & UWA Coding Bootcamp** - for design guides
+
+* **Luca & Sam** - for their time in teaching HTML, CSS and Javascript
+
+
+
---
+++ script.js
@@ -0,0 +1,82 @@
+// sync the slider range input with the number input
+let rangeOfCharacter = document.getElementById('range-of-characters');
+let numberOfCharacter = document.getElementById('number-of-characters');
+
+rangeOfCharacter.addEventListener('input', sync)
+numberOfCharacter.addEventListener('input', sync)
+
+function sync(x){
+    let value = x.target.value
+    rangeOfCharacter.value = value
+    numberOfCharacter.value = value
+}
+// 
+
+// grouping each type of characters into its own category
+let includeUppercase = document.getElementById('include-uppercase');
+let includeNumerics = document.getElementById('include-numerics');
+let includeSymbols = document.getElementById('include-symbols');
+let passwordBtn = document.getElementById('generate-password-btn');
+// 
+
+// create array for lowercase, uppercase, numerics and symbols
+// using ASCII character codes
+function createArray(a,b){
+    let charactersArray = [];
+    for (let i = a; i <= b ; i++){
+        charactersArray.push(i);
+    }
+    return charactersArray
+}
+
+let lowercaseArray = createArray(97,122);
+let uppercaseArray = createArray(65,90);
+let numericsArray = createArray(48,57);
+let symbolsArray = createArray(33,47).concat(
+                    createArray(58,64)).concat(
+                    createArray(91,96)).concat(
+                    createArray(123,126));
+// 
+
+// create function for generating random password
+function generatePassword(amountChar,uppercaseChar, numericsChar, symbolsChar){
+    let genPassword = lowercaseArray;
+    if (uppercaseChar){
+        genPassword = genPassword.concat(uppercaseArray);
+    }
+    if (numericsChar){
+        genPassword = genPassword.concat(numericsArray);
+    }
+    if (symbolsChar){
+        genPassword = genPassword.concat(symbolsArray);
+    }
+
+    let passwordArray = []
+    for (let i = 0; i < amountChar; i++){
+        let length = genPassword.length;
+        let character = genPassword[Math.floor(Math.random() * length)]
+        passwordArray.push(String.fromCharCode(character))
+    }
+    return passwordArray.join('')
+}
+// 
+
+// create event when clicking the generate password button to return result
+let generatedPassword = document.getElementById('generated-password');
+
+passwordBtn.addEventListener("click", function(event){
+    event.preventDefault()
+    console.log(event);
+
+    
+let amountChar = parseFloat(rangeOfCharacter.value)
+let uppercaseChar = includeUppercase.checked    
+let numericsChar = includeNumerics.checked
+let symbolsChar = includeSymbols.checked
+
+    let password = generatePassword(amountChar,uppercaseChar, numericsChar, symbolsChar)
+    generatedPassword.innerText = password;
+})
+// 
+
+
